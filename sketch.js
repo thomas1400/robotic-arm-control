e75aaa76c9a7e2e31bfab9ed1d23900a515f683f
Thomas Owens <thomasowens2000@gmail.com>
2019-10-24 16:33:11 -0400

    Improved visualization and added boundary checking.

--- sketch.js
+++ sketch.js
@@ -2,21 +2,20 @@ let q0 = 0; // angle about the z axis, relative to positive x
 let q1 = 45; // between horizontal plane of base and first arm segment
 let q2 = 90; // between first arm segment and second arm segment
 
+let target;
 let origin;
 let p1; // top of base
 let p2; // elbow
 let p3; // top of claw
 let p4; // base of claw
 
-let long_length = 200;
-let short_length = 50;
+const long_length = 200;
+const short_length = 50;
 
-let width = 4.1 * long_length;
-let height = 2.5 * long_length;
+const width = 4.1 * long_length;
+const height = 2.5 * long_length;
 
-let target;
-
-let speed = 3;
+const speed = 3;
 
 let camera_angle = 1;
 let camera_height = -250;
@@ -40,7 +39,7 @@ function draw() {
 
   // draw the xz-plane
   push()
-  rotateX(HALF_PI, 0, 0);
+  rotateX(HALF_PI);
   fill(100);
   strokeWeight(0);
   ambientMaterial(150);
@@ -88,33 +87,37 @@ function draw() {
 
   // Draw lines
   stroke(255);
-  strokeWeight(5);
+  strokeWeight(8);
 
   line(origin.x, origin.y, origin.z, p1.x, p1.y, p1.z);
   line(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
   line(p2.x, p2.y, p2.z, p3.x, p3.y, p3.z);
   line(p3.x, p3.y, p3.z, p4.x, p4.y, p4.z);
 
+  // Draw shadow
+  stroke(100);
+  strokeWeight(5);
+  line(origin.x, 0, origin.z, target.x, 0, target.z);
+
   if (keyIsDown(LEFT_ARROW)) {
-    target.x -= speed;
+    if (checkMotion(0, -speed)) target.x -= speed;
   }
   if (keyIsDown(RIGHT_ARROW)) {
-    target.x += speed;
+    if (checkMotion(0, speed)) target.x += speed;
   }
   if (keyIsDown(UP_ARROW)) {
-    target.y -= speed;
+    if (checkMotion(1, -speed)) target.y -= speed;
   }
   if (keyIsDown(DOWN_ARROW)) {
-    target.y += speed;
+    if (checkMotion(1, speed)) target.y += speed;
   }
   if (keyIsDown(188)) {
-    target.z -= speed;
+    if (checkMotion(2, -speed)) target.z -= speed;
   }
   if (keyIsDown(190)) {
-    target.z += speed;
+    if (checkMotion(2, speed)) target.z += speed;
   }
 
-  // works
   if (keyIsDown(65)) { // a
     camera_angle += 0.05;
   }
@@ -149,4 +152,16 @@ function rotateVectorY(vector, theta) {
   productz = round(m[2][0] * vector.x + m[2][1] * vector.y + m[2][2] * vector.z);
 
   return createVector(productx, producty, productz);
+}
+
+function checkMotion(axis, m) {
+  check = target.copy();
+
+  if (axis == 0) check.x += m;
+  if (axis == 1) check.y += m;
+  if (axis == 2) check.z += m;
+
+  d = dist(check.x, check.y, check.z, origin.x, origin.y, origin.z);
+  r = dist(check.x, check.z, origin.x, origin.z);
+  return d < 2 * long_length - speed && check.y < 0 && r > long_length / 4;
 }
